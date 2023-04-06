c9a2b1e2f804a54acfffe5fc4f5f0981b30c0e32
haesa <xdhaesa@gmail.com>
2023-04-06 13:52:43 +0900

    refactor(App): this.game을 private field로 변경

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ const Game = require('./components/game');
 const { START } = require('./constant/constant');
 
 class App {
+  #game;
   constructor() {
-    this.game = new Game();
+    this.#game = new Game();
   }
 
   play() {
     Console.print(START);
-    this.game.start();
+    this.#game.start();
   }
 }
 module.exports = App;
