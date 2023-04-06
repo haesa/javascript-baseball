3690b87357c3ddeb32687e11aabbb32ab8135f91
haesa <xdhaesa@gmail.com>
2023-04-06 17:24:25 +0900

    refactor(baseballNumber): constructor에서 초기화
    인스턴스를 생성하자마자 초기화 하도록 변경

--- src/components/computer.js
+++ src/components/computer.js
@@ -3,23 +3,19 @@ const { Random } = require('@woowacourse/mission-utils');
 class Computer {
   #baseballNumber;
   constructor() {
-    this.#baseballNumber;
-  }
-
-  init() {
-    this.#baseballNumber = [];
+    this.#baseballNumber = this.pickNumber();
   }
 
   pickNumber() {
-    this.init();
-
-    while (this.#baseballNumber.length < 3) {
+    const result = [];
+    while (result.length < 3) {
       const number = Random.pickNumberInRange(1, 9);
 
-      if (!this.#baseballNumber.includes(number)) {
-        this.#baseballNumber.push(number);
+      if (!result.includes(number)) {
+        result.push(number);
       }
     }
+    return result;
   }
 
   get baseballNumber() {
--- src/components/game.js
+++ src/components/game.js
@@ -103,7 +103,6 @@ class Game {
 
   start() {
     this.computer = new Computer();
-    this.computer.pickNumber();
     this.readAnswer();
   }
 }
